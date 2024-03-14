c9592577a5694f81192782f4509e4d44a3640ffc
WangChowder <dpeas611@gmail.com>
2024-03-14 10:11:09 +0800

    change

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -12,13 +12,14 @@ function Footer() {
           <br />
           <div className="container mb-5 d-flex justify-content-between">
               <div className="d-flex flex-column justify-content-space-evenly">
-                  <p><i className="fa-solid fa-phone"></i> {"電話:04-229374948"}</p>
-                  <p><i className="fa-solid fa-house"></i> {"地址:408台中市南屯區公益路二段51號18樓"}</p>
-                  <p><i className="fa-solid fa-envelope"></i> {"電子信箱:goodcare@gmail.com"}</p>
-                  <p><i className="fas fa-clock"></i> {"營業時間:0900-1800"}</p>
+                  <p><i className="fa-solid fa-phone"></i> {"電話: 04-229374948"}</p>
+                  <p><i className="fa-solid fa-house"></i> {"地址: 408台中市南屯區公益路二段51號18樓"}</p>
+                  <p><i className="fa-solid fa-envelope"></i> {"電子信箱: goodcare@gmail.com"}</p>
+                  <p><i className="fas fa-clock"></i> {"營業時間: 09:00-18:00"}</p>
               </div>
               <div className="d-flex flex-column">
                   <div><img src={require("../../asset/images/index/footerlogo.png")} alt='footerLogo'/></div>
+                  <br/>
                   <div className="d-flex justify-content-between">
                       <a href="https://www.facebook.com/mohw.gov.tw/" target="_blank" rel="noreferrer">
                           <h1><i className="fa-brands fa-facebook text-white"></i></h1>
