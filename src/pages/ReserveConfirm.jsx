1504da1cb09852a0fe7918d90c79978edd0dfc40
chien-hui37 <momo050307@gmail.com>
2024-03-17 22:46:39 +0800

    add reserveconfirm 

---
+++ src/pages/ReserveConfirm.jsx
@@ -0,0 +1,23 @@
+import React, { } from 'react';
+import Header from '../components/header/Header';
+import IndexNav from '../components/indexnav/IndexNav';
+import PathBox from '../components/pathbox/PathBox';
+import Footer from '../components/footer/Footer';
+import BackToTopBtn from '../components/backtotopbtn/BackToTopBtn';
+import ReserveConfirm from '../components/reserve/ReserveConfirm';
+
+const Service = () => {
+  
+  return (
+    <React.Fragment>
+        <Header />
+        <IndexNav />
+        <PathBox />
+        <ReserveConfirm />
+        <Footer />
+        <BackToTopBtn />
+      </React.Fragment>
+  )
+}
+
+export default Service
