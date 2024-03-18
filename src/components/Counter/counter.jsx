d15b211487c6600296ed0b19174a2fe8ed2e0984
e/ <chaochunyang36@gmail.com>
2024-03-18 16:54:04 +0800

    cart update

--- src/components/Counter/counter.jsx
+++ src/components/Counter/counter.jsx
@@ -1,38 +1,63 @@
-import React, { useState } from 'react'
+import React, { useState,useCallback } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const Counter = ( { childToParent } ) => {
-    const [counter, setCounter] = useState(1)
+const Counter = ( { curNum, childToParent } ) => {
+    const [counter, setCounter] = useState(curNum)
 
-    var doPlus = () => {
-        var newcounuter = 0
-        if (counter < 10){
-            newcounuter = counter +1
-            setCounter(newcounuter)
-            childToParent(newcounuter)            
-        }else{
-            // 給定庫存最大數?
-            newcounuter = 10
-            setCounter(newcounuter)
-            childToParent(newcounuter)          
-        }
+    var doPlus = useCallback(() => {
+      if (counter < 10){
+        setCounter(prev=>{
+          const newCounter = prev + 1;
+          childToParent(newCounter);
+          return newCounter;
+        })
+      } else {
+        setCounter(10)
+        childToParent(counter)
+      }
+    },[counter, childToParent])
+
+    var doReduce = useCallback(() =>{
+      if (counter > 1){
+        setCounter(prev=>{
+          const newCounter = prev - 1;
+          childToParent(newCounter);
+          return newCounter;
+        })
+      } else {
+        setCounter(1)
+        childToParent(counter)
+      }
+    },[counter,childToParent])
+    // var doPlus = () => {
+    //     var newcounuter = 0
+    //     if (counter < 10){
+    //         newcounuter = counter +1
+    //         setCounter(newcounuter)
+    //         childToParent(newcounuter)            
+    //     }else{
+    //         // 給定庫存最大數?
+    //         newcounuter = 10
+    //         setCounter(newcounuter)
+    //         childToParent(newcounuter)          
+    //     }
         
-    }
+    // }
     
-    var doReduce = () => {
-        var newcounuter = 0
-        if(counter > 1) {
-            newcounuter = counter -1
-            setCounter(newcounuter)
-            childToParent(newcounuter)          
-        }else{
-            newcounuter = 1
-            setCounter(newcounuter)
-            childToParent(newcounuter)          
-        }
+    // var doReduce = () => {
+    //     var newcounuter = 0
+    //     if(counter > 1) {
+    //         newcounuter = counter -1
+    //         setCounter(newcounuter)
+    //         childToParent(newcounuter)          
+    //     }else{
+    //         newcounuter = 1
+    //         setCounter(newcounuter)
+    //         childToParent(newcounuter)          
+    //     }
         
-    }
+    // }
     
     return(
         <React.Fragment>
--- src/components/cartdetail/CartDetail.jsx
+++ src/components/cartdetail/CartDetail.jsx
@@ -1,115 +1,156 @@
 import React, { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import { useForm } from "react-hook-form"
 import "./CartDetail.css";
-
-import toast, { Toaster } from 'react-hot-toast';
 import Counter from "../Counter/counter";
 
-const CartDetail = ( { doSecondBtn } ) => {
-  let cartInfo = JSON.parse(sessionStorage.getItem('cartInfo'))
-  let [userCoupon, setUserCoupon] = useState('')
+const CartDetail = ({ doSecondBtn }) => {
+  let sessionInfo = JSON.parse(sessionStorage.getItem("cartInfo"));
+  const [ cartInfo, setCartInfo ] = useState(sessionInfo||[]);
+  const [ couponInd, setCouponInd ] = useState(0);
+  const [ userCoupon, setUserCoupon ] = useState(null);
   
-  // 吐司提示
-  const notify = () => toast.success('購物車尚未有任何東西！')
-  if(!cartInfo) notify()
-
-  // 計算總金額
-  let cartPrice = 0
-  let charge = 100
-  let totalDiscount = 0
-  let totalPrice = 0
-
-  let realCoupon = [{code:'GoodCare2024',discount:0.9},{code:'GC2024',discount:0.85}]
+  console.log(userCoupon)
+  // 更新購物車資訊
+  const changeCartInfo = (id, num) => {
+    const updatedCartInfo = cartInfo.map(item => {
+      if (item.id === id) {
+        return { ...item, quantity: num };
+      }
+      return item;
+    });
+    setCartInfo(updatedCartInfo);
+  };
   
-  let couponInd = realCoupon.findIndex((i)=>i.code === userCoupon)
-
-  cartPrice = cartInfo? cartInfo.map((v)=> cartPrice += v.quantity * v.price) : 0
-  totalPrice = ( couponInd>=0 )? cartPrice * realCoupon[couponInd].discount + charge : cartPrice + charge
-  totalDiscount = totalPrice - cartPrice - charge
-
-  const doSubmitCode = (e) => {
-    // setUserCoupon(e.target.value)
-    console.log('OK')
+  // 跳出吐司提示
+  const notify = (msg) => toast.success(msg);
+  if (!cartInfo.length) notify("購物車尚未有任何東西！");
+  if (userCoupon) notify("使用折扣碼成功！")
+  
+  // 驗證折扣碼
+  let realCoupon = [
+    { code: "GC2024", discount: 0.85 },
+    { code: "GodRex", discount: 0.7}
+  ];
+  const { register, handleSubmit } = useForm();
+  const onSubmit = e => {
+    let checkInd = realCoupon.findIndex(i=>i.code===e.code)
+    if (checkInd>=0) setUserCoupon(realCoupon[checkInd].discount)
+    console.log(e.code)
   }
+  const checkCoupon = e => {
+    let checkInd = realCoupon.findIndex(i=>i.code===e.target.value)
+    setCouponInd(checkInd)
+    console.log("coupon id:",couponInd)
+  }
+
+  
+  // 計算總金額
+  let cartPrice = 0;
+  let charge = 100;
+  let totalDiscount = 0;
+  let totalPrice = 0;
+
+
+  cartPrice = cartInfo.length>0
+    ? cartInfo.reduce((total,item)=>{
+      return total + item.quantity*item.price
+    },0)
+    :0
+  totalPrice =
+    userCoupon
+      ? cartPrice * userCoupon + charge
+      : cartPrice + charge;
+  totalDiscount = totalPrice - cartPrice - charge;
 
   return (
     <>
       {/* 購物車明細 */}
       <div className="container justify-content-center">
-        <table className="table" style={{width:"100%"}}>
+        <table className="table" style={{ width: "100%" }}>
           <thead>
             <tr>
               <th scope="col">商品圖片</th>
               <th scope="col">商品名稱</th>
               <th scope="col">單價</th>
               <th scope="col">數量</th>
-              <th scope="col">折扣</th>
               <th scope="col">小計</th>
               <th scope="col">刪除</th>
             </tr>
           </thead>
           <tbody>
-            {cartInfo? cartInfo.map((val,ind)=>{
-              return(
-                <tr>
-                  <td>
+            {cartInfo.length>0 ? (
+              cartInfo.map( val => {
+                return (
+                  <tr key={ val.id }>
+                    <td>
                       <img
-                      src={require(`../../asset/images/numberImages/${val.id}.png`)}
-                      alt="Product"
-                      width="150"
-                      height="150"
-                    />
-                  </td>
-                  <td>
-                    { val.productName }
-                  </td>
-                  <td>
-                    { val.price }
-                  </td>
-                  <td>
-                    <Counter />
-                  </td>
-                  <td>
-                    <select className="form-select">
-                      <option value="0">0%</option>
-                      <option value="0.1">10%</option>
-                      <option value="0.2">20%</option>
-                    </select>
-                  </td>
-                  <td>
-                    總價還沒計算
-                  </td>
-                </tr>
-              )
-            })
-            :<tr><td colSpan={ 7 } className="text-muted text-center"> 您尚未加入任何東西到購物車 </td></tr>
-            }     
+                        src={require(`../../asset/images/numberImages/${val.id}.png`)}
+                        alt="Product"
+                        width="150"
+                        height="150"
+                      />
+                    </td>
+                    <td>{val.productName}</td>
+                    <td>{val.price}</td>
+                    <td>
+                      <Counter 
+                      curNum={ val.quantity } 
+                      childToParent={ numChanged => {changeCartInfo(val.id, numChanged)} }
+                      />
+                    </td>
+                    <td>{ val.quantity*val.price }</td>
+                  </tr>
+                );
+              })
+            ) : (
+              <tr>
+                <td colSpan={6} className="text-muted text-center">
+                  {" "}
+                  您尚未加入任何東西到購物車{" "}
+                </td>
+              </tr>
+            )}
           </tbody>
         </table>
       </div>
 
       {/* 折扣碼 */}
-      {cartInfo &&
+      {cartInfo.length>0 && (
         <div className="container">
-        <div className="row">
-              <div className="col-3"></div>
-              <div className="col-6">
+          <div className={ couponInd<0? "block":"d-none"} >
+            <p className="text-danger">此折扣碼無效</p>
+          </div>
+          <div className="justify-content-center">
+          <div className="col-6">
+              <form className="input-group" onSubmit={handleSubmit(onSubmit)}>
                 <input
                   type="text"
                   className="form-control"
                   placeholder="請輸入折扣碼"
+                  {...register('code',
+                    {
+                      require:true,
+                      pattern:'^/w+$',
+                      maxLength:6
+                    }
+                  )}
+                  onChange={ checkCoupon }
                 />
-              </div>
-              <div className="col-3">
-                <button className="btn btn-outline-primary" type="button" onChange={ doSubmitCode }>
-                  使用折扣碼
-                </button>
-              </div>
+                <div className="input-group-append">
+                  <input type="submit" 
+                  className="btn btn-outline-primary" 
+                  value="使用折扣碼"
+                  />
+                </div>
+              </form>
+          </div>
+          </div>
+          <Toaster />
         </div>
-        <br />
-        </div>
-      }
+      )}
       {/* 帳單明細 */}
-      {cartInfo &&
+      {cartInfo.length>0 && (
         <div className="container">
           <div className="row">
             <div className="col-3"></div>
@@ -121,44 +162,49 @@ const CartDetail = ( { doSecondBtn } ) => {
                 <tbody>
                   <tr>
                     <td>商品小計</td>
-                    <td>{ 'NT$' + cartPrice }</td>
+                    <td>{"NT$" + cartPrice}</td>
                   </tr>
                   <tr>
                     <td>折扣</td>
-                    <td className={ (totalDiscount<0)? "text-danger":"" }>{ 'NT$' + totalDiscount }</td>
+                    <td className={totalDiscount < 0 ? "text-danger" : ""}>
+                      {"NT$" + totalDiscount}
+                    </td>
                   </tr>
                   <tr>
                     <td>運費</td>
-                    <td>{ 'NT$' + charge }</td>
+                    <td>{"NT$" + charge}</td>
                   </tr>
                   <tr>
                     <td>合計</td>
-                    <td>{ 'NT$' + totalPrice }</td>
+                    <td>{"NT$" + totalPrice}</td>
                   </tr>
                 </tbody>
               </table>
               <br />
               <div className="text-center">
-                  <button type="button" className="btn btn-lg custom-button px-5" onClick={ doSecondBtn }>
-                    前往結帳
-                  </button>
+                <button
+                  type="button"
+                  className="btn btn-lg custom-button px-5"
+                  onClick={doSecondBtn}
+                >
+                  前往結帳
+                </button>
               </div>
             </div>
           </div>
         </div>
-      }
+      )}
       {/* 叫他回去買東西 */}
-      {!cartInfo &&
+      {cartInfo.length===0 && (
         <div className="grid text-center">
           <a href="/product">
             <button className="btn btn-lg custom-button px-5 g-start-2">
               <i className="fa-solid fa-shopping-cart"></i> 繼續購物
             </button>
           </a>
-          <Toaster position="top-right" reverseOrder={false}/>
+          <Toaster position="top-right" reverseOrder={false} />
         </div>
-
-      }
+      )}
     </>
   );
 };
