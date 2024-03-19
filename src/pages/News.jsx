f5aff89fcffb2229c6c62a9c43a1bb122a7ae57c
e/ <chaochunyang36@gmail.com>
2024-03-19 13:47:26 +0800

    news & video

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -42,21 +42,48 @@ function TemplateNews() {
 
               {/* 這裡放你的主內容 */}
               <div className='col-9'>
-                  <ul class="nav nav-tabs" id="myTab" role="tablist">
+                  <ul className="nav nav-tabs" id="myTab" role="tablist">
                     <li>
-                        <button class="nav-link" style={{  backgroundColor: '#214F39',color: 'white',textShadow:'2px 2px 4px rgba(0, 0, 0, 0.5)'}}>焦點新聞</button>
+                        <button className="nav-link" style={{  backgroundColor: '#214F39',color: 'white',textShadow:'2px 2px 4px rgba(0, 0, 0, 0.5)'}}>焦點新聞</button>
                     </li>
                   </ul>
                   <div className="tab-content " id="myTabContent">
                     <div className="tab-pane fade show active" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabIndex="0">
-                      <div className="mb-4 new-content">
+                      <div className="mb-4 new-content container">
                       {currentNews.map((val,ind) => {
                           return (
                             <>
-                              <div className="newsitem text-truncate" key={ind}>
-                                <a href="#a" target="_blank" className='newslink'>
-                                {val.title}
-                                </a>
+                              <div className="newsitem row" key={ind}>
+                                <div className='col-10 text-truncate'>
+                                  <a type="button" className='newslink' data-bs-toggle="modal" href={"#Modal"+ind}>
+                                  {val.title}
+                                  </a>
+                                </div>
+                                <div className='col-2 text-muted'>
+                                  { val.update }
+                                </div>
+                              </div>
+                              <div className="modal fade" id={"Modal"+ind} tabindex="-1" aria-labelledby={"ModalLabel"+ind} aria-hidden="true">
+                                <div className="modal-dialog modal-md">
+                                  <div className="modal-content">
+                                    <div className="modal-header">
+                                      <h5 className="modal-title" id={"Modal"+ind}>{ val.title }</h5>
+                                      <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div className="modal-body">
+                                      <div className='text-muted'><p>來源：{ val.source }</p></div>
+                                      <div className='text-muted'><p>更新日期：{ val.update }</p></div>
+                                      <br />
+                                      新聞內容：
+                                      <section>
+                                      { val.content }
+                                      </section>
+                                    </div>
+                                    <div className="modal-footer">
+                                      <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    </div>
+                                  </div>
+                                </div>
                               </div>
                             </>
                           ) 
