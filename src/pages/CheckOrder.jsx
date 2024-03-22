83eddd9f4810d2b5b335f054082a58d3ec5c7a64
e/ <chaochunyang36@gmail.com>
2024-03-22 15:29:04 +0800

    check order page

---
+++ src/pages/CheckOrder.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Header from '../components/header/Header'
+import IndexNav from '../components/indexnav/IndexNav'
+import Footer from '../components/footer/Footer'
+import BackToTopBtn from "../components/backtotopbtn/BackToTopBtn";
+
+const CheckOrder = () => {
+  return (
+    <>
+      <Header />
+      <IndexNav />
+      <br />
+      <br />
+      <br />
+      <Footer />
+      <BackToTopBtn />
+    </>
+  )
+}
+
+export default CheckOrder
